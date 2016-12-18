353d36edb8f9e21eb504cad14fba99f228d5bb96
Kozar Vladyslav <k13vlad@gmail.com>
2016-12-18 14:29:28 +0200

    second commit update massiv

--- src/massiv.java
+++ src/massiv.java
@@ -2,33 +2,25 @@
 public class massiv {
 
     public static void main(String[] args) {
-       int a[] = {0,1,2,3,4,5};
+//      Иниализация и вывод массива
 
+        int a[] = {0,1,2,3,4,5};
+        System.out.println("Array: ");
         for (int t = 0; t < a.length; t++){
-            System.out.println(a[t] + "\n");
+            System.out.println(a[t]);
         }
 
-        double r = 0;
-        for (int i = 0; i < a.length; i++) {
-            r = r + a[i];
-        }
-        double m = r / a.length;
-        System.out.println(m);
+//      Вывод среднего значения
+        double m = averageValue(a);
+        System.out.println("\n" + "Average Value " + m);
 
+//      Вывод минимума и максимума массива
+        int b[] = minmax(a);
+        System.out.println("Minimum value: " + b[0] + " & " +  "Maximum value: " + b[1] + "\n");
 
-        int max = a[0];
-        int min = a[0];
-        for(int i = 0; i != a.length; i ++){
-            if(a[i] > max){
-                max = a[i];
-            }
-            if(a[i] < min){
-                min = a[i];
-            }
-        }
-        System.out.println(min + " & " + max + "\n");
+//      Вывод обратного массива
+        System.out.println("Reverse array:");
         Reverse(a);
-
         for (int j = 0; j < a.length; j++){
             System.out.println(a[j]);
         }
@@ -47,7 +39,32 @@ public class massiv {
         return a;
     }
 
+    public static double averageValue(int[]a){
+        double r = 0;
+        for (int i = 0; i < a.length; i++) {
+            r = r + a[i];
+        }
+        r = r / a.length;
+        return r;
+    }
 
+    public static int[] minmax(int[]a){
+
+        int max = a[0];
+        int min = a[0];
+        for(int i = 0; i != a.length; i ++){
+            if(a[i] > max){
+                max = a[i];
+            }
+            if(a[i] < min){
+                min = a[i];
+            }
+        }
+        int []mmas = {min, max};
+
+        return mmas;
+
+    }
 
 }
 
