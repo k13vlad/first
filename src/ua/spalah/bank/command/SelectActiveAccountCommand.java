c066da05e9dca86be2d126262832f774154516ad
Kozar Vladyslav <k13vlad@gmail.com>
2017-01-28 15:42:07 +0200

    All commands.

--- src/ua/spalah/bank/command/SelectActiveAccountCommand.java
+++ src/ua/spalah/bank/command/SelectActiveAccountCommand.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Scanner;
 
 import ua.spalah.bank.models.Account;
+import ua.spalah.bank.services.AccountType;
 
 
 public class SelectActiveAccountCommand implements Command {
@@ -12,7 +13,12 @@ public class SelectActiveAccountCommand implements Command {
     @Override
     public void execute() {
         List<Account> accountList = BankCommander.currentClient.getAccounts();
-        System.out.println("Choose active account and enter number of account");
+        System.out.println("Choose active account and enter number of account\n");
+        for (int i = 0; i < accountList.size(); i++) {
+            int counter = i + 1;
+            System.out.println("Account number: " + counter);
+            counter = 0;
+        }
         int accountNumber = in.nextInt();
         BankCommander.currentClient.setActiveAccount(accountList.get(accountNumber - 1));
     }
